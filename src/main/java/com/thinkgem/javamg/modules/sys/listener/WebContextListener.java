9645f3151077c35973e897b542d4ce5fa4ee773f
Mr.Lin <553515787@qq.com>
2016-04-15 18:03:41 +0800

    1

--- src/main/java/com/thinkgem/javamg/modules/sys/listener/WebContextListener.java
+++ src/main/java/com/thinkgem/javamg/modules/sys/listener/WebContextListener.java
@@ -6,13 +6,16 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.thinkgem.javamg.modules.sys.service.SystemService;
 
+/**
+ * 自定义的ContextListener 继承 org.springframework.web.context.ContextLoaderListener
+ */
 public class WebContextListener extends org.springframework.web.context.ContextLoaderListener {
 	
 	@Override
 	public WebApplicationContext initWebApplicationContext(ServletContext servletContext) {
-		if (!SystemService.printKeyLoadMessage()){
+		if (!SystemService.printKeyLoadMessage()){/* 在项目context启动之前加载我们的参数,但是好像这里没干啥 */
 			return null;
 		}
-		return super.initWebApplicationContext(servletContext);
+		return super.initWebApplicationContext(servletContext);//spring的context初始化开始
 	}
 }
