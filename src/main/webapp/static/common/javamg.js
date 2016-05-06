0557458815bd72097c4a759d27c606ab5e5f99d9
Mr.Lin <553515787@qq.com>
2016-05-06 09:30:10 +0800

    1

--- src/main/webapp/static/common/javamg.js
+++ src/main/webapp/static/common/javamg.js
@@ -61,7 +61,7 @@ function getDictLabel(data, value, defaultValue){
 // 打开一个窗体
 function windowOpen(url, name, width, height){
 	var top=parseInt((window.screen.height-height)/2,10),left=parseInt((window.screen.width-width)/2,10),
-		options="location=no,menubar=no,toolbar=no,dependent=yes,minimizable=no,modal=yes,alwaysRaised=yes,"+
+		options="location=no,menubar=no,toolbar=no,depended=yes,minimizable=no,modal=yes,alwaysRaised=yes,"+
 		"resizable=yes,scrollbars=yes,"+"width="+width+",height="+height+",top="+top+",left="+left;
 	window.open(url ,name , options);
 }
